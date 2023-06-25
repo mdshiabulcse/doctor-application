929fa7d0c5e09d261c053ebe75e94564d9259bd3
shiabulislam <md.shiabul.cse@gmail.com>
2023-06-26 01:50:28 +0600

    vuetify 3 iinstal for project login panale

--- frontend/src/template.js
+++ frontend/src/template.js
@@ -10,3 +10,24 @@ import "./assets/js/accordion.js";
 import "./assets/js/venobox.js";
 import "./assets/js/slick.js";
 import "./assets/js/main.js";
+
+
+import "@/dashboard_assets/assets/js/jquery-3.6.0.min.js";
+
+import "@/dashboard_assets/assets/js/bootstrap.bundle.min.js";
+
+import "@/dashboard_assets/assets/js/feather.min.js";
+
+import "@/dashboard_assets/assets/plugins/select2/js/select2.min.js";
+
+import "@/dashboard_assets/assets/plugins/slimscroll/jquery.slimscroll.min.js";
+
+import "@/dashboard_assets/assets/plugins/owl-carousel/owl.carousel.min.js";
+
+import "@/dashboard_assets/assets/plugins/apexchart/apexcharts.min.js";
+import "@/dashboard_assets/assets/plugins/apexchart/chart-data.js";
+
+// import "@/dashboard_assets/assets/js/moment.min.js";
+// import "@/dashboard_assets/assets/plugins/daterangepicker/daterangepicker.js";
+
+import "@/dashboard_assets/assets/js/script.js";
