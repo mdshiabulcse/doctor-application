ff10b2efaa2d009bd59b09164a4cb4b701074b7d
shiabulislam <md.shiabul.cse@gmail.com>
2023-06-26 19:22:53 +0600

    vue tify 3 install

---
+++ vuetify-app/src/router/index.js
@@ -0,0 +1,28 @@
+// Composables
+import { createRouter, createWebHistory } from 'vue-router'
+import DashboardHome from "@/views/dashboard/DashboardHome.vue";
+
+const routes = [
+  {
+    path: '/',
+    // component: () => import('@/layouts/default/Default.vue'),
+    children: [
+      {
+        path: '',
+        name: 'Home',
+        component: () => import('@/views/auth/Login.vue'),
+      },
+    ],
+  },
+  {
+        path: '/dashboard', name: 'Dashboard', component: DashboardHome,
+
+  },
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
+})
+
+export default router
