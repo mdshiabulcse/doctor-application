8b6cb7b371a6ea5b0f79b7819e32f42ecb2b1816
shiabulislam <md.shiabul.cse@gmail.com>
2023-07-07 23:09:08 +0600

    csrf token mismatch , add traits in laravel

---
+++ frontend/src/vuetify.js
@@ -0,0 +1,15 @@
+import { createApp } from 'vue';
+import { Vuetify } from 'vuetify';
+import 'vuetify/dist/vuetify.min.css';
+
+import '@mdi/font/css/materialdesignicons.css';
+
+export function setupVuetify(app) {
+    const vuetify = new Vuetify();
+
+    app.use(vuetify);
+}
+
+const app = createApp(App);
+setupVuetify(app);
+app.mount('#app');
