29d3c124dde28fc0ae9cfb11cb74c280676e7549
shiabulislam <md.shiabul.cse@gmail.com>
2023-07-09 23:50:59 +0600

    nice admin dahboard add

--- frontend/src/template.js
+++ frontend/src/template.js
@@ -1,14 +1,26 @@
-import "./assets/vendor/bootstrap/jquery-1.12.4.min.js";
-import "./assets/vendor/bootstrap/popper.min.js";
-import "./assets/vendor/bootstrap/bootstrap.min.js";
-import "./assets/vendor/niceselect/nice-select.min.js";
-import "./assets/vendor/slickslider/slick.min.js";
-import "./assets/vendor/venobox/venobox.min.js";
-import "./assets/js/nice-select.js";
-import "./assets/js/countdown.js";
-import "./assets/js/accordion.js";
-import "./assets/js/venobox.js";
-import "./assets/js/slick.js";
-import "./assets/js/main.js";
+// import "./assets/vendor/bootstrap/jquery-1.12.4.min.js";
+// import "./assets/vendor/bootstrap/popper.min.js";
+// import "./assets/vendor/bootstrap/bootstrap.min.js";
+// import "./assets/vendor/niceselect/nice-select.min.js";
+// import "./assets/vendor/slickslider/slick.min.js";
+// import "./assets/vendor/venobox/venobox.min.js";
+// import "./assets/js/nice-select.js";
+// import "./assets/js/countdown.js";
+// import "./assets/js/accordion.js";
+// import "./assets/js/venobox.js";
+// import "./assets/js/slick.js";
+// import "./assets/js/main.js";
+
+import "@/dashboard_assets/assets/vendor/apexcharts/apexcharts.min.js";
+import "@/dashboard_assets/assets/vendor/bootstrap/js/bootstrap.bundle.min.js";
+import "@/dashboard_assets/assets/vendor/chart.js/chart.umd.js";
+import "@/dashboard_assets/assets/vendor/echarts/echarts.min.js";
+import "@/dashboard_assets/assets/vendor/quill/quill.min.js";
+import "@/dashboard_assets/assets/vendor/simple-datatables/simple-datatables.js";
+import "@/dashboard_assets/assets/vendor/tinymce/tinymce.min.js";
+import "@/dashboard_assets/assets/vendor/php-email-form/validate.js";
+
+// <!-- Template Main JS File -->
+import "@/dashboard_assets/assets/js/main.js";
 
 
