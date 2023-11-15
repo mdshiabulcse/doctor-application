d231e615f00e0f25fc73d195309a9c4310b4ca7b
shiabulislam <md.shiabul.cse@gmail.com>
2023-11-15 10:46:17 +0600

    login cridential error problem not solved

--- vuetify-app/src/store/auth.js
+++ vuetify-app/src/store/auth.js
@@ -27,7 +27,7 @@ export const useAuth = defineStore('auth',{
         }
       }catch (error){
         if (error.response.data){
-          // this.errors =error.response.data.errors;
+          this.errors =error.response.data.errors;
           return new Promise((reject)=>{
             reject(error.response.data.errors);
           })
--- vuetify-app/src/views/auth/Login.vue
+++ vuetify-app/src/views/auth/Login.vue
@@ -17,12 +17,18 @@ const toggleShow = () => {
 };
 
 const onSubmit = async (values, {setErrors}) => {
+  if (auth.errors){
+    notify.Error(auth.errors);
+  }
   const res = await auth.login(values);
+
   if (res.data) {
     router.push({name: 'user.dashboard'});
     notify.Success('Login Successfully!');
   } else {
+
     setErrors(res);
+
   }
 
 };
