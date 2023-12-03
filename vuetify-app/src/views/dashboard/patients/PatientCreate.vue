17ac76296e5d77ed33c89e52b6baedd457283983
shiabulislam <md.shiabul.cse@gmail.com>
2023-12-04 00:25:20 +0600

    patient create birth date pending

--- vuetify-app/src/views/dashboard/patients/PatientCreate.vue
+++ vuetify-app/src/views/dashboard/patients/PatientCreate.vue
@@ -10,64 +10,72 @@
           </v-breadcrumbs>
         </v-sheet>
       </v-col>
- <v-col cols="7" >
-   <form @submit.prevent="submit">
-     <v-text-field
-       v-model="name.value.value"
-       :counter="10"
-       :error-messages="name.errorMessage.value"
-       label="Name"
-     ></v-text-field>
+      <v-col cols="7">
+        <v-card
+          class="mx-auto my-2"
+          title="Patient Create"
+          prepend-icon="mdi-36px mdi-light mdi-clipboard-text-outline"
+          rel="noopener"
+          color="warning"
+        ></v-card>
+        <form @submit.prevent="submit">
+          <v-text-field
+            v-model="name.value.value"
+            :counter="10"
+            :error-messages="name.errorMessage.value"
+            label="Name"
+          ></v-text-field>
 
-     <v-text-field
-       v-model="phone.value.value"
-       :counter="7"
-       :error-messages="phone.errorMessage.value"
-       label="Phone Number"
-     ></v-text-field>
+          <v-text-field
+            v-model="phone.value.value"
+            :counter="7"
+            :error-messages="phone.errorMessage.value"
+            label="Phone Number"
+          ></v-text-field>
 
-     <v-text-field
-       v-model="email.value.value"
-       :error-messages="email.errorMessage.value"
-       label="E-mail"
-     ></v-text-field>
+          <v-text-field
+            v-model="email.value.value"
+            label="E-mail"
+          ></v-text-field>
+          <v-text-field
+            v-model="selectedDate"
+            label="Select Date"
+            prepend-icon="mdi-calendar"
+            readonly
+          ></v-text-field>
 
-     <v-select
-       v-model="select.value.value"
-       :items="items"
-       :error-messages="select.errorMessage.value"
-       label="Select"
-     ></v-select>
+          <v-date-picker v-model="selectedDate" ></v-date-picker>
 
-     <v-checkbox
-       v-model="checkbox.value.value"
-       :error-messages="checkbox.errorMessage.value"
-       value="1"
-       label="Option"
-       type="checkbox"
-     ></v-checkbox>
+          <v-select
+            v-model="select.value.value"
+            :items="items"
+            :error-messages="select.errorMessage.value"
+            label="Reference"
+          ></v-select>
 
-     <v-btn
-       class="me-4"
-       type="submit"
-     >
-       submit
-     </v-btn>
+          <v-btn prepend-icon="mdi-check-circle"
+                 class="me-4"
+                 type="submit"
+                 color="primary"
+          >
+            submit
+          </v-btn>
 
-     <v-btn @click="handleReset">
-       clear
-     </v-btn>
-   </form>
- </v-col>
-  <v-col cols="5">
+          <v-btn prepend-icon="mdi-trash-can-outline" @click="handleReset">
+            clear
+          </v-btn>
+        </form>
+      </v-col>
+      <v-col cols="5">
 
- </v-col>
+      </v-col>
     </v-row>
   </v-container>
 </template>
 <script setup>
 import {computed, ref} from 'vue'
-import { useField, useForm } from 'vee-validate'
+import {useField, useForm} from 'vee-validate'
+
 const breadcrumbs = computed(() => [
   {
     title: 'Home',
@@ -80,29 +88,29 @@ const breadcrumbs = computed(() => [
     href: '#',
   },
 ]);
-const { handleSubmit, handleReset } = useForm({
+const {handleSubmit, handleReset} = useForm({
   validationSchema: {
-    name (value) {
+    name(value) {
       if (value?.length >= 2) return true
 
       return 'Name needs to be at least 2 characters.'
     },
-    phone (value) {
+    phone(value) {
       if (value?.length > 9 && /[0-9-]+/.test(value)) return true
 
       return 'Phone number needs to be at least 9 digits.'
     },
-    email (value) {
+    email(value) {
       if (/^[a-z.-]+@[a-z.-]+\.[a-z]+$/i.test(value)) return true
 
       return 'Must be a valid e-mail.'
     },
-    select (value) {
+    select(value) {
       if (value) return true
 
       return 'Select an item.'
     },
-    checkbox (value) {
+    checkbox(value) {
       if (value === '1') return true
 
       return 'Must be checked.'
@@ -113,7 +121,7 @@ const name = useField('name')
 const phone = useField('phone')
 const email = useField('email')
 const select = useField('select')
-const checkbox = useField('checkbox')
+
 
 const items = ref([
   'Item 1',
@@ -125,4 +133,17 @@ const items = ref([
 const submit = handleSubmit(values => {
   alert(JSON.stringify(values, null, 2))
 })
+const selectedDate = ref(null);
+
+const openDatePicker = () => {
+  // Open the date picker when the text field is clicked
+  // You can customize the behavior based on your requirements
+  selectedDate.value = true;
+};
+
+const closeDatePicker = () => {
+  // Close the date picker when a date is selected
+  // You can customize the behavior based on your requirements
+  selectedDate.value = false;
+};
 </script>
