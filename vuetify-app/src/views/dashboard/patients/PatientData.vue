52cf11ef83b61e3e080b22f40400a7d380f41089
shiabulislam <md.shiabul.cse@gmail.com>
2023-12-23 11:08:44 +0600

    invoice page ongoing

--- vuetify-app/src/views/dashboard/patients/PatientData.vue
+++ vuetify-app/src/views/dashboard/patients/PatientData.vue
@@ -56,21 +56,6 @@
             </v-icon>
           </template>
         </v-data-table>
-        <v-dialog v-model="deleteDialog" max-width="400">
-          <v-card>
-            <v-card-title class="headline">
-              Delete Confirmation
-            </v-card-title>
-            <v-card-text>
-              Are you sure you want to delete this item?
-            </v-card-text>
-            <v-card-actions>
-              <v-btn color="primary" @click="deleteItem">Yes</v-btn>
-              <v-btn color="red" @click="deleteDialog = false">No</v-btn>
-            </v-card-actions>
-          </v-card>
-        </v-dialog>
-
       </v-col>
     </v-row>
   </v-container>
@@ -90,7 +75,7 @@ const desserts = ref([]);
 // const userData = useAuth();
 // const user_id = userData.user.data.id;
 // const dialogEdit = ref(false);
-const deleteDialog = ref(false);
+
 const loading = ref(false);
 const router = useRouter();
 
