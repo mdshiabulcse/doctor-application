692fed5bc5ec08867d59f7d3ae91cc6ef3be42bb
shiabulislam <md.shiabul.cse@gmail.com>
2024-01-24 00:25:13 +0600

    time zone setup pending

--- vuetify-app/src/services/globalUrlConfig.js
+++ vuetify-app/src/services/globalUrlConfig.js
@@ -3,5 +3,6 @@ import { ref } from 'vue';
 
 // Set your default URL here
 const localUrl = ref('http://127.0.0.1:8000');
+// const localUrl = ref('https://docapi.wetrustcoders.com');
 
 export { localUrl };
