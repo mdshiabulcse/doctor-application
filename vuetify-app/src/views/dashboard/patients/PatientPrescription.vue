762ed20c79cc041b0c9bb62dea624fddbd158dba
shiabulislam <md.shiabul.cse@gmail.com>
2024-02-09 11:14:00 +0600

    prescription create as new

--- vuetify-app/src/views/dashboard/patients/PatientPrescription.vue
+++ vuetify-app/src/views/dashboard/patients/PatientPrescription.vue
@@ -1,148 +1,154 @@
 <template>
-     <v-row>
-       <v-col>
-         <v-col cols="12">
-           <v-card
-             :loading="loading"
-           >
-             <v-container>
-               <v-row >
-                 <v-col cols="4">
-                   <v-autocomplete
-                     v-model="submitForm.previous_prescription_id"
-                     :items="previous_prescription"
-                     label="Previous Prescription"
-                     :item-title="formatPreviousPrescriptionTitle"
-                     item-value="id"
-                     @change="updateFormFromPreviousPrescription"
-                   ></v-autocomplete>
-                 </v-col>
-                 <v-col cols="4">
-                   <v-btn class="ma-2" color="success" @click="submit"> {{ buttonText }}</v-btn>
-                   <v-btn class="ma-2" prepend-icon="mdi-printer" color="primary"> Print</v-btn>
-                 </v-col>
-               </v-row>
-             </v-container>
-           </v-card>
-         </v-col>
-           <v-row>
-                  <v-col cols="3">
-                    <v-card class="pa-2 ma-2">
-                      <v-card-text>
-                          <v-row>
-                            <v-col>
-                              <v-card
-                                class="mx-auto"
-                                max-width="100%"
-                                color="primary"
-                                variant="flat"
-                              >
-                                <v-card-item>
-                                  <div v-if="appointment_info">
-                                    <div class="text-overline mb-1">
-                                      <span >Name: {{appointment_info.patient_info.patient_name}}</span>
-                                    </div>
-                                    <div class="text-overline mb-1">
-                                      <span>ID: {{appointment_info.patient_id}}</span>
-                                    </div>
-                                    <div class="text-overline mb-1">
-                                      <span>Doctor: {{appointment_info.doctor_info.doctor_name}}</span>
-                                    </div>
-                                  </div>
-                                </v-card-item>
-                              </v-card>
-                            </v-col>
-                            <v-col cols="12">
-                              <v-text-field
-                                v-model="submitForm.symptoms"
-                                required
-                                minlength="2"
-                                label="Symptoms"
-                              ></v-text-field>
-                            </v-col>
-                            <v-col cols="12">
-                              <v-autocomplete
-                                v-model="submitForm.examination_data"
-                                required
-                                minlength="2"
-                                label="Examination(Pathology)"
-                              ></v-autocomplete>
-                            </v-col>
-                            <v-col cols="12">
-                              <v-textarea
-                                v-model="submitForm.advice_note"
-                                required
-                                label="Advice Note"
-                              ></v-textarea>
-                            </v-col>
-                            <v-col cols="12">
-                              <v-text-field
-                                v-model="submitForm.followup_date"
-                                label="Followup Date"
-                                type="date"
-                              >
-                              </v-text-field>
-                            </v-col>
-                          </v-row>
-                      </v-card-text>
-                    </v-card>
-                  </v-col>
-                  <v-col>
-                    <v-card class="pa-2 ma-2">
-                      <v-card-text>
-                        <v-row v-for="(medicine, index) in submitForm.medicines" :key="index">
-                          <v-col cols="2">
-                            <v-autocomplete
-                              v-model="medicine.type"
-                              required
-                              label="Type"
-                              :items="medicine_type"
-                            ></v-autocomplete>
-                          </v-col>
-                          <v-col cols="3">
-                            <v-autocomplete
-                              v-model="medicine.medicine_id"
-                              :items="medicine_data"
-                              required
-                              label="Medicine"
-                              item-value="id"
-                              item-title="medicine_name"
-                            ></v-autocomplete>
-                          </v-col>
-                          <v-col cols="2">
-                            <v-text-field
-                              v-model="medicine.duration"
-                              required
-                              label="Duration"
-                            ></v-text-field>
-                          </v-col>
-                          <v-col cols="3">
-                            <v-text-field
-                              v-model="medicine.medicine_instruction"
-                              required
-                              label="Instruction"
-                            ></v-text-field>
-                          </v-col>
-                          <v-col v-if="submitForm.medicines.length > 1" cols="1">
-                            <v-btn icon="mdi-minus" color="error" density="compact" @click="removeMedicine(index)"></v-btn>
-                          </v-col>
-                          <v-col cols="1">
-                            <v-btn  icon="mdi-plus" color="warning"  density="compact" @click="addMedicine"></v-btn>
-                          </v-col>
-                        </v-row>
-                      </v-card-text>
-                    </v-card>
-                  </v-col>
-           </v-row>
-       </v-col>
-     </v-row>
+  <v-row>
+    <v-col>
+      <v-col cols="12">
+        <v-card
+          :loading="loading"
+        >
+          <v-container>
+            <v-row>
+              <v-col cols="4">
+                <v-autocomplete
+                  v-model="submitForm.previous_prescription_id"
+                  :items="previous_prescription"
+                  label="Previous Prescription"
+                  :item-title="formatPreviousPrescriptionTitle"
+                  item-value="id"
+                  @change="updateFormFromPreviousPrescription"
+                ></v-autocomplete>
+              </v-col>
+              <v-col v-if="showActionButtons" cols="4">
+                <v-btn class="ma-2" color="success" @click="submit">{{ buttonText }}</v-btn>
+                <v-btn class="ma-2" prepend-icon="mdi-printer" color="primary" @click="PrintPrescription">Print</v-btn>
+              </v-col>
+              <v-col v-else cols="4">
+                <v-btn class="ma-2" color="primary" @click="createNewPrescription">Create as New</v-btn>
+                <v-btn class="ma-2" prepend-icon="mdi-printer" color="info" @click="PrintPrescription">Print</v-btn>
+              </v-col>
+            </v-row>
+          </v-container>
+        </v-card>
+      </v-col>
+      <v-row>
+        <v-col cols="3">
+          <v-card class="pa-2 ma-2">
+            <v-card-text>
+              <v-row>
+                <v-col>
+                  <v-card
+                    class="mx-auto"
+                    max-width="100%"
+                    color="primary"
+                    variant="flat"
+                  >
+                    <v-card-item>
+                      <div v-if="appointment_info">
+                        <div class="text-overline mb-1">
+                          <span>Name: {{ appointment_info.patient_info.patient_name }}</span>
+                        </div>
+                        <div class="text-overline mb-1">
+                          <span>ID: {{ appointment_info.patient_id }}</span>
+                        </div>
+                        <div class="text-overline mb-1">
+                          <span>Doctor: {{ appointment_info.doctor_info.doctor_name }}</span>
+                        </div>
+                      </div>
+                    </v-card-item>
+                  </v-card>
+                </v-col>
+                <v-col cols="12">
+                  <v-text-field
+                    v-model="submitForm.symptoms"
+                    required
+                    minlength="2"
+                    label="Symptoms"
+                  ></v-text-field>
+                </v-col>
+                <v-col cols="12">
+                  <v-autocomplete
+                    v-model="submitForm.examination_data"
+                    required
+                    minlength="2"
+                    label="Examination(Pathology)"
+                  ></v-autocomplete>
+                </v-col>
+                <v-col cols="12">
+                  <v-textarea
+                    v-model="submitForm.advice_note"
+                    required
+                    label="Advice Note"
+                  ></v-textarea>
+                </v-col>
+                <v-col cols="12">
+                  <v-text-field
+                    v-model="submitForm.followup_date"
+                    label="Followup Date"
+                    type="date"
+                  >
+                  </v-text-field>
+                </v-col>
+              </v-row>
+            </v-card-text>
+          </v-card>
+        </v-col>
+        <v-col>
+          <v-card class="pa-2 ma-2">
+            <v-card-text>
+              <v-row v-for="(medicine, index) in submitForm.medicines" :key="index">
+                <v-col cols="2">
+                  <v-autocomplete
+                    v-model="medicine.type"
+                    required
+                    label="Type"
+                    :items="medicine_type"
+                  ></v-autocomplete>
+                </v-col>
+                <v-col cols="3">
+                  <v-autocomplete
+                    v-model="medicine.medicine_id"
+                    :items="medicine_data"
+                    required
+                    label="Medicine"
+                    item-value="id"
+                    item-title="medicine_name"
+                  ></v-autocomplete>
+                </v-col>
+                <v-col cols="2">
+                  <v-text-field
+                    v-model="medicine.duration"
+                    required
+                    label="Duration"
+                  ></v-text-field>
+                </v-col>
+                <v-col cols="3">
+                  <v-text-field
+                    v-model="medicine.medicine_instruction"
+                    required
+                    label="Instruction"
+                  ></v-text-field>
+                </v-col>
+                <v-col v-if="submitForm.medicines.length > 1" cols="1">
+                  <v-btn icon="mdi-minus" color="error" density="compact" @click="removeMedicine(index)"></v-btn>
+                </v-col>
+                <v-col cols="1">
+                  <v-btn icon="mdi-plus" color="warning" density="compact" @click="addMedicine"></v-btn>
+                </v-col>
+              </v-row>
+            </v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-col>
+  </v-row>
 </template>
 <script setup>
-import { onMounted, ref,computed ,watch} from 'vue';
+import {onMounted, ref, computed, watch} from 'vue';
 import axiosInstance from "@/services/axiosService";
-import { useNotification } from "@/store/notification";
-import { useAuth } from "@/store/auth";
+import {useNotification} from "@/store/notification";
+import {useAuth} from "@/store/auth";
 import {useRoute, useRouter} from "vue-router";
+import {localUrl} from "@/services/globalUrlConfig";
+
 const router = useRouter();
 const loading = ref(true);
 const notify = useNotification();
@@ -155,15 +161,15 @@ const user_id = useAuth().user.data.id;
 const routeParams = useRoute().params;
 const app_id = routeParams.app_id;
 const patient_id = routeParams.pid;
-const prescription_id = routeParams.prescriptionId !== 'null' ? parseInt(routeParams.prescriptionId) : 0 ;
+const prescription_id = routeParams.prescriptionId !== 'null' ? parseInt(routeParams.prescriptionId) : 0;
 
 const medicine_type = ref(['Tab', 'Cap', 'Drop', 'Syrup', 'Inj']);
 const submitForm = ref({
-  prescription_id:prescription_id,
-  previous_prescription_id:'',
-  app_id:app_id,
+  prescription_id: prescription_id,
+  previous_prescription_id: '',
+  app_id: app_id,
   user_id: user_id,
-  doctor_id:'',
+  doctor_id: '',
   symptoms: '',
   examination_data: '',
   advice_note: '',
@@ -188,18 +194,18 @@ const fetchAppointmentInfo = async () => {
     const response = await axiosInstance(`/admin/appointment/patient-appointment-info/${app_id}`);
     appointment_info.value = response.data.appointment_info;
     submitForm.value.doctor_id = appointment_info.value.doctor_id;
-    loading.value=false;
+    loading.value = false;
   } catch (error) {
     console.error('Error fetching data:', error);
   }
 };
 
-const fetchPreviousPrescription =async ()=>{
+const fetchPreviousPrescription = async () => {
   try {
     const response = await axiosInstance(`/admin/prescription/previous-prescription/${patient_id}`);
     previous_prescription.value = response.data.previous_prescription;
-  }catch (error){
-    console.error('error ',error)
+  } catch (error) {
+    console.error('error ', error)
   }
 }
 
@@ -216,7 +222,7 @@ watch(() => submitForm.value.previous_prescription_id, async (newValue, oldValue
 
 const updateFormFromPreviousPrescription = async (newValue) => {
   try {
-    await router.push({ path: `/patient-prescription/${app_id}/${appointment_info.value.patient_id}/${newValue}` });
+    await router.push({path: `/patient-prescription/${app_id}/${appointment_info.value.patient_id}/${newValue}`});
     submitForm.value.prescription_id = newValue;
     await fetchPrescriptionData();
   } catch (error) {
@@ -225,12 +231,31 @@ const updateFormFromPreviousPrescription = async (newValue) => {
   }
 };
 
+
 const buttonText = computed(() => {
-  return prescription_id ? "Update" : "Save";
+  if (prescription_id === 0) {
+    return "Save"; // New prescription, show "Save" button
+  } else {
+    // Check if it's the same day as the prescription creation date
+    const selectedPrescription = previous_prescription.value.find(prescription => prescription.id === submitForm.value.previous_prescription_id);
+    const isTodayPrescription = selectedPrescription && new Date(selectedPrescription.create_date).toDateString() === new Date().toDateString();
+    return isTodayPrescription ? "Update" : "Create as New"; // Show "Update" or "Create as New" button based on condition
+  }
 });
+
+const showActionButtons = computed(() => {
+  if (prescription_id === 0) {
+    return true; // New prescription, show action buttons
+  } else {
+    const selectedPrescription = previous_prescription.value.find(prescription => prescription.id === submitForm.value.previous_prescription_id);
+    const isTodayPrescription = selectedPrescription && new Date(selectedPrescription.create_date).toDateString() === new Date().toDateString();
+    return isTodayPrescription;
+  }
+});
+
 const fetchPrescriptionData = async () => {
   try {
-    if (prescription_id  !== 0) {
+    if (prescription_id !== 0) {
       const response = await axiosInstance.get(`/admin/prescription/prescription/${submitForm.value.prescription_id}`);
       prescription_data.value = response.data.prescription_data;
       prescription_medicine_data.value = response.data.prescription_medicine_data;
@@ -289,7 +314,7 @@ const submit = async () => {
     if (response.data.message) {
       const prescriptionId = response.data.prescription_id;
       // Navigate to the same route with different parameters
-      await router.push({ path: `/patient-prescription/${app_id}/${appointment_info.value.patient_id}/${prescriptionId}` });
+      await router.push({path: `/patient-prescription/${app_id}/${appointment_info.value.patient_id}/${prescriptionId}`});
       notify.Success(response.data.message);
       // Fetch updated data after the route change
       await fetchPrescriptionData();
@@ -302,6 +327,35 @@ const submit = async () => {
   } catch (error) {
     notify.Error(error.response.data.errors);
   }
+};
+const createNewPrescription = async () => {
+  try {
+    let response;
+
+    // Save and create new prescription as old wise
+    response = await axiosInstance.post('/admin/prescription/prescription?patient_id=' + appointment_info.value.patient_id, submitForm.value);
+
+    if (response.data.message) {
+      const prescriptionId = response.data.prescription_id;
+      // Navigate to the same route with different parameters
+      await router.push({path: `/patient-prescription/${app_id}/${appointment_info.value.patient_id}/${prescriptionId}`});
+      notify.Success(response.data.message);
+      // Fetch updated data after the route change
+      await fetchPrescriptionData();
+
+      // Reload the page
+      window.location.reload();
+    } else {
+      notify.Error(response.data.errors);
+    }
+  } catch (error) {
+    notify.Error(error.response.data.errors);
+  }
+};
+
+
+const PrintPrescription = () => {
+  window.open(localUrl.value + `/print/prescription-print/${patient_id}/${prescription_id}` , '_blank');
 };
 
 </script>
